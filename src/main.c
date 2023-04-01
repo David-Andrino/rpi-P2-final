5cadedd910f5a6788cfa0b0a16fdda9754b3c133
David Andrino <davidandrino2002@gmail.com>
2023-04-01 11:51:57 -0400

    Basic thread structure

--- src/main.c
+++ src/main.c
@@ -5,41 +5,98 @@
  *      Author: David Andrino & Fernando Sanz
  */
 #include <stdio.h>
-#include "Accelerometer/accelerometer.h"
-#include "ColorSensor/colorSensor.h"
 #include <stdint.h>
 #include <signal.h>
+#include <pthread.h>
+#include <unistd.h>
 
-static int stop = 0;
+#include "Accelerometer/accelerometer.h"
+#include "ColorSensor/colorSensor.h"
+
+static volatile int stop = 0;
+static pthread_mutex_t stopMutex  = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t accMutex   = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t colorMutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t stopCond    = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t accCond     = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t colorCond   = PTHREAD_COND_INITIALIZER;
 
 void closeISR(int signal) {
 	stop = 1;
 }
 
+void* accThFn(void *ptr) {
+	int accStop = 0;
+	while (1) {
+		pthread_mutex_lock(&stopMutex);
+		accStop = stop;
+		pthread_mutex_unlock(&stopMutex);
+
+		if (accStop) break;
+	}
+	pthread_exit(NULL);
+}
+
+void* inputThFn(void *ptr) {
+	int inputStop = 0;
+	while (1) {
+		// Check stop flag
+		pthread_mutex_lock(&stopMutex);
+		inputStop = stop;
+		pthread_mutex_unlock(&stopMutex);
+
+		if (inputStop) break;
+	}
+	pthread_exit(NULL);
+}
+
+void* colorThFn(void *ptr) {
+	int colorStop = 0;
+	while (1) {
+		// Check stop flag
+		pthread_mutex_lock(&stopMutex);
+		colorStop = stop;
+		pthread_mutex_unlock(&stopMutex);
+
+		if (colorStop) break;
+	}
+	pthread_exit(NULL);
+}
+
+void* displayThFn(void *ptr) {
+	int displayStop = 0;
+	while (1) {
+		// Check stop flag
+		pthread_mutex_lock(&stopMutex);
+		displayStop = stop;
+		pthread_mutex_unlock(&stopMutex);
+
+		if (displayStop) break;
+	}
+	pthread_exit(NULL);
+}
+
 int main(int argc, char** argv) {
+	pthread_t accTID, colorTID, displayTID, inputTID; 
+
 	signal(SIGINT, closeISR);
 
-	acc_t acceleration;
-	rgbColor_t color;
+	pthread_create(accTID,     NULL, accThFn,     NULL);
+	pthread_create(colorTID,   NULL, colorThFn,   NULL);
+	pthread_create(inputTID,   NULL, inputThFn,   NULL);
+	pthread_create(displayTID, NULL, displayThFn, NULL);
 
-	cs_init();
-	acc_init();
 
-	printf("Sensors - David Andrino & Fernando Sanz\n\n\n\n\n\n");
-	while (!stop) {
-	cs_readClearCorrectedColor(&color);
-	acc_read(&acceleration);
+	// Wait for stop to be activated
+	pthread_mutex_lock(&stopMutex);
+		while (stop == 0) pthread_cond_wait(&stopCond, &stopMutex);
+	pthread_mutex_unlock(&stopMutex);
 
-	printf("\033[F\033[F\033[F\033[F\033[F\r"
-			"Color: R,G,B: (%03d, %03d, %03d)\n"
-			"Acceleration:\n"
-			"    X: %03f\n"
-			"    Y: %03f\n"
-			"    Z: %03f\n",
-			color.r, color.g, color.b, acceleration.x, acceleration.y, acceleration.z);
+	// Wait for the threads to exit
+	pthread_join(accTID,     NULL);
+	pthread_join(colorTID,   NULL);
+	pthread_join(inputTID,   NULL);
+	pthread_join(displayTID, NULL);
 
-	}
-	cs_close();
-	acc_close();
-	return 0;
+	exit(0);
 }
